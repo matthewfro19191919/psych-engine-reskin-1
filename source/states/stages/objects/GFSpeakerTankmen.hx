77b439969bcc24db191475218c25a07d9729e5e6
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-03-25 16:06:07 -0400

    Update GFSpeakerTankmen.hx

--- source/states/stages/objects/GFSpeakerTankmen.hx
+++ source/states/stages/objects/GFSpeakerTankmen.hx
@@ -1,13 +1,13 @@
 package states.stages.objects;
 
-import graphics.FlxSpriteExt;
 import states.PlayState;
 
 class GFSpeakerTankmen extends FlxSpriteGroup
 {
 	var speaker:FlxAnimate;
         var body:FlxAnimate;
-        var body:FlxAnimate;
+	var head1:FlxAnimate;
+	var head2:FlxAnimate;
 
 public function new(x:Float = 0, y:Float = 0)
 {	
@@ -16,33 +16,35 @@ public function new(x:Float = 0, y:Float = 0)
 	speaker.anim.addBySymbol('anim', 'speakers', 24, false);
 	speaker.anim.play('anim', true);
 	speaker.anim.curFrame = speaker.anim.length - 1;
-	speaker.antialiasing = antialias;
 	add(speaker);
-    
-    for (i in 0...2) {
+	
 	body = new FlxAnimate(speaker.x, speaker.y - 85);
 	Paths.loadAnimateAtlas(body, 'characters/speakers/tankmanBodyPart');
-	body.anim.addBySymbol('anim', 'speakers', 24, false);
+	body.anim.addBySymbol('anim', 'tankmanBodyPart', 24, false);
 	body.anim.play('anim', true);
 	body.anim.curFrame = speaker.anim.length - 1;
-	body.antialiasing = antialias;
 	add(body);
 
-        var headStr = 'tankmanTop'+ (i + 1);
-        var head:FlxAnimate = new FlxAnimate(speaker.x, speaker.y - 215);
-	Paths.loadAnimateAtlas('characters/speakers/' + headStr);
-        head.x += (i != 0 ? -120 : 445);
-	head.anim.addBySymbol('anim', 'speakers', 24, false);
-	head.anim.play('anim', true);
-	head.anim.curFrame = speaker.anim.length - 1;
-	head.antialiasing = antialias;
-	add(head);
-    }
+        head1 = new FlxAnimate(speaker.x, speaker.y - 215);
+	Paths.loadAnimateAtlas(head1, 'characters/speakers/tankmanTop1');
+	head1.anim.addBySymbol('anim', 'tankmanTop1', 24, false);
+	head1.anim.play('anim', true);
+	head1.anim.curFrame = speaker.anim.length - 1;
+	add(head1);
+
+        head2 = new FlxAnimate(speaker.x, speaker.y - 215);
+	Paths.loadAnimateAtlas(head2, 'characters/speakers/tankmanTop2');
+	head2.anim.addBySymbol('anim', 'tankmanTop2', 24, false);
+	head2.anim.play('anim', true);
+	head2.anim.curFrame = speaker.anim.length - 1;
+	add(head2);
+
 }
 public function beatHit()
 {
 	speaker.anim.play('anim', true);
 	body.anim.play('anim', true);
-	head.anim.play('anim', true);
+	head1.anim.play('anim', true);
+	head2.anim.play('anim', true);
 }
 }
