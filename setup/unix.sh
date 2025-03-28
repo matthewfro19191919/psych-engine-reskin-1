07bc3bd9c45c45f7e21389ad2062f484783ec047
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-03-28 06:20:16 -0400

    Update unix.sh

--- setup/unix.sh
+++ setup/unix.sh
@@ -9,7 +9,7 @@ echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
 haxelib git hxcpp https://github.com/MobilePorting/hxcpp --quiet
 haxelib git lime https://github.com/MobilePorting/lime --quiet
-haxelib git openfl https://github.com/MobilePorting/openfl 9.3.3 --quiet
+haxelib git openfl https://github.com/MobilePorting/openfl --quiet
 haxelib git flixel https://github.com/MobilePorting/flixel 5.6.1 --quiet
 haxelib install flixel-addons 3.2.2 --quiet
 haxelib install flixel-tools 1.5.1 --quiet
